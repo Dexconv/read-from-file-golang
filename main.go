733c22459fcf2f74fba547edf8a95ab8e7bbc85b
Ali Naghiee <Ali.16148@gmail.com>
2020-05-07 14:04:34 +0430

    initialized git

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+var tpl *template.Template
+
+func init(){
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+func main(){
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080",nil)
+}
+func index(w http.ResponseWriter, r *http.Request){
+	var s string
+	if r.Method == http.MethodPost{
+
+		f, h, err := r.FormFile("q")
+
+		if err != nil{
+			http.Error(w, http.StatusText(405), 405)
+		}
+
+		defer f.Close()
+
+		fmt.Println("\nfile:", f, "\nheader:", h, "\nerr", err)
+
+		bs, err := ioutil.ReadAll(f)
+		if err != nil{
+			http.Error(w, http.StatusText(500), 500)
+			log.Fatalln(err)
+		}
+		s = string(bs)
+
+		dst, err := os.Create(filepath.Join("./storage/",h.Filename))
+		if err != nil{
+			http.Error(w, http.StatusText(500), 500)
+			log.Fatalln(err)
+		}
+		defer dst.Close()
+		_, err = dst.Write(bs)
+		if err != nil{
+			http.Error(w, http.StatusText(500), 500)
+			log.Fatalln(err)
+		}
+	}
+	tpl.ExecuteTemplate(w,"index.gohtml", s)
+}
